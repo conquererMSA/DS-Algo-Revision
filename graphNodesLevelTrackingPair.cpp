5bc5f9ed5c081584e500e9e0063b697522ce11b7
asus test <msa-99@outlook.com>
2024-10-13 20:40:04 +0600

    findDirectConnection

--- graphNodesLevelTrackingPair.cpp
+++ graphNodesLevelTrackingPair.cpp
@@ -3,7 +3,8 @@ using namespace std;
 vector<int> graph[1005];
 bool visitedNode[1005];
 int nodeLevel[1005];
-void trackinNodeLevelByBFS(int sourceNode){
+pair<bool, int> isConnected({false,NULL});
+void trackinNodeLevelByBFS(int sourceNode, int destinationNode){
     queue<pair<int,int>> q;
     q.push({sourceNode,0});
     nodeLevel[sourceNode]=0;
@@ -13,7 +14,10 @@ void trackinNodeLevelByBFS(int sourceNode){
         int parentNode=parent.first;
         int parentNodeLevel=parent.second;
         q.pop();
-        // cout<<parentNode<<" ";
+        if(parentNode==destinationNode){
+            isConnected.first=true;
+            isConnected.second=parentNodeLevel;
+        }
         for(int child:graph[parentNode]){
             if(!visitedNode[child]){
                 nodeLevel[child]=nodeLevel[parentNode]+1;
@@ -31,11 +35,13 @@ int main(){
         graph[v].push_back(u);
     }
     int sourceNode; cin>>sourceNode; //zodi graph e source node na thake tahole source node ti print hoye/kaj kore queue loop bondho zabe. karon graph e kuno node na thake tar child o thakbe na.
+    int destinationNode; cin>>destinationNode;
     memset(visitedNode, false, sizeof(visitedNode));
     memset(nodeLevel, -1, sizeof(nodeLevel));
-    trackinNodeLevelByBFS(sourceNode);
-    for(int i=0; i<n; i++){
-        cout<<"node "<<i<<" level "<<nodeLevel[i]<<endl;
+    trackinNodeLevelByBFS(sourceNode,destinationNode);
+    if(isConnected.first){
+        cout<<"YES, nodes "<<destinationNode<<" level is "<<isConnected.second<<endl;
     }
+    else cout<<"No"<<endl;
     return 0;
 }
