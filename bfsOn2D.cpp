d185278bd931d8f82ecfbd1798ed39dcc362e4e1
asus test <msa-99@outlook.com>
2024-10-14 02:26:11 +0600

    componentsCountByDFS

--- bfsOn2D.cpp
+++ bfsOn2D.cpp
@@ -1,34 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 char d2Grid[100][100];
 bool visitedNode[100][100];
 int n, m;
 vector<pair<int, int>> childPath = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
 bool isValid(int row, int col) {
     return (row >= 0 && row < n && col >= 0 && col < m);
 }
-void bfsOn2DGrid(int sourceRow, int sourceCol){
+
+void bfsOn2DGrid(int sourceRow, int sourceCol) {
     queue<pair<int, int>> q;
     q.push({sourceRow, sourceCol});
-    visitedNode[sourceRow][sourceCol]=true;
-    while(!q.empty()){
-        pair<int, int> parent=q.front();
+    visitedNode[sourceRow][sourceCol] = true;
+
+    while (!q.empty()) {
+        pair<int, int> parent = q.front();
         q.pop();
-        int parentRow=parent.first;
-        int parentCol=parent.second;
-        //ekhane unvisited and new node asbe.
-        cout<<d2Grid[parentRow][parentCol]<<" ";
-        for(int i=0; i<4; i++){
-            int childRow=parentRow+childPath[i].first;
-            int childCol=parentCol+childPath[i].second;
-            if(isValid(childRow, childCol) && !visitedNode[childRow][childCol]){
+        int parentRow = parent.first;
+        int parentCol = parent.second;
+
+        cout << d2Grid[parentRow][parentCol] << " ";
+
+        for (int i = 0; i < 4; i++) {
+            int childRow = parentRow + childPath[i].first;
+            int childCol = parentCol + childPath[i].second;
+
+            if (isValid(childRow, childCol) && !visitedNode[childRow][childCol]) {
                 q.push({childRow, childCol});
-                visitedNode[childRow][childCol]=true;
+                visitedNode[childRow][childCol] = true;
             }
         }
     }
 }
-int main(){
+
+int main() {
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
@@ -37,10 +44,21 @@ int main(){
     }
 
     memset(visitedNode, false, sizeof(visitedNode));
+
     int sourceRow, sourceCol;
     cin >> sourceRow >> sourceCol;
-    bfsOn2DGrid(sourceRow, sourceCol);
-    if(visitedNode[3][4]) cout<<"YES"<<endl;
-    else cout<<"NO"<<endl;
+
+    if (isValid(sourceRow, sourceCol)) {
+        bfsOn2DGrid(sourceRow, sourceCol);
+
+        if (visitedNode[3][4]) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    } else {
+        cout << "Invalid source node." << endl;
+    }
+
     return 0;
 }
--- componentsOfGraph.cpp
+++ componentsOfGraph.cpp
@@ -1,32 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 vector<int> graph[100];
 bool visitedNode[100];
-void componentCountByDFS(int sourceNode){
-    cout<<sourceNode<<" ";
-    visitedNode[sourceNode]=true;
-    for(int child:graph[sourceNode]){
-        if(!visitedNode[child]){
+
+void componentCountByDFS(int sourceNode) {
+    visitedNode[sourceNode] = true;
+    for (int child : graph[sourceNode]) {
+        if (!visitedNode[child]) {
             componentCountByDFS(child);
         }
     }
 }
-int main(){
-    int n,e;
-    cin>>n>>e;
-    while(e--){
-        int u,v; cin>>u>>v;
+
+int main() {
+    int n, e;
+    cin >> n >> e;
+    for (int i = 0; i < e; i++) {
+        int u, v;
+        cin >> u >> v;
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
-    int numberOfComponent=0;
-    for(int i=0; i<6; i++){
-        if(!visitedNode[i]){
+
+    int numberOfComponent = 0;
+    for (int i = 0; i < n; i++) {
+        if (!visitedNode[i]) {
             componentCountByDFS(i);
             numberOfComponent++;
         }
     }
-    cout<<endl;
-    cout<<numberOfComponent<<endl;
+
+    cout << numberOfComponent << endl;
     return 0;
 }
