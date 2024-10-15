e40c81787548a39a4571f69192552b3be1046ead
asus test <msa-99@outlook.com>
2024-10-16 01:49:58 +0600

    equal sum partition

---
+++ equalSumPartition.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    int sum=0;
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+        sum+=a[i];
+
+    }
+    // int s; cin>>s;
+    // cin>>s; //equal sum partion e sum banate hobe.
+    if(sum%2==0){
+        int s=sum/2;
+    bool dp[n+1][s+1];
+    dp[0][0]=true;
+    for(int i=1; i<=s; i++){
+        dp[0][i]=false;
+    }
+    pair<int,int> cell;
+    //i=1, j=0 hobe karon array value 0 hote pare.
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<=s; j++){
+            if(a[i-1]<=j){
+                dp[i][j]=dp[i-1][j-a[i-1]]||dp[i-1][j];
+            }
+            else {
+                dp[i][j]=dp[i-1][j];
+            }
+            //protom kunkun value use kore sum create kora possibel sei cell store kora hoyeche
+            // if(j==s && dp[i][j]==1){
+            //     cell.first=i;
+            //     cell.second=j;
+            // }
+        }
+    }
+    //koyta value use kore kotovabe sum creat kora possible ta dp[i][j] theke jana jay.
+    // for(int i=0; i<=n; i++){
+    //     for(int j=0; j<=s; j++){
+    //         cout<<dp[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<cell.first<<" "<<cell.second<<endl;
+    //array er sob gulu value use kore s koto vabe create kora possible ta dp[n][s] teke jana jay.
+    if(dp[n][s]) cout<<dp[n][s]<<endl;
+    }
+    else cout<<"Not Possible"<<endl;
+    return 0;
+}
