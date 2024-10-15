8bd43e9568acaad9284eb5eae0f6ab2e375fc6cc
asus test <msa-99@outlook.com>
2024-10-16 03:22:35 +0600

    ball

--- ball.cpp
+++ ball.cpp
@@ -1,17 +1,37 @@
 #include<bits/stdc++.h>
+#define ll long long
+#define B begin()
+#define E end()
+#define all(x) x.B,x.E
+#define fast() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
 using namespace std;
-int main(){
-    int n; cin>>n;
-    set<int> balls;
-    for(int i=0; i<n; i++){
-       int x;
-       cin>>x;
-       balls.insert(x);
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
     }
-    int ans=0;
-    for(auto it=balls.begin(); it!=balls.end(); it++){
-         ans+=*it;
+    sort(all(vec), greater<>());
+
+    map<int, bool> mp;
+
+    int64_t sum = 0;
+    int crr = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        if (crr > 0) {
+            crr = min(crr - 1, vec[i]);
+            sum += crr;
+        }
     }
-    cout<<ans<<endl;
+    cout << sum << endl;
+}
+int main()
+{
+    fast();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
     return 0;
 }
